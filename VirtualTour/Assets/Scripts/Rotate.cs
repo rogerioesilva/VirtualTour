31461755bb8da37a29ed6acf7ef389bdbfd0bfe1
rogerioesilva <professor.rogerio@gmail.com>
2019-12-17 19:39:43 -0600

    Stilized Water

---
+++ VirtualTour/Assets/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float RotationalSpeed = 5f;
+    public Transform referencePoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(referencePoint.position, Vector3.up, RotationalSpeed * Time.deltaTime);
+    }
+}
