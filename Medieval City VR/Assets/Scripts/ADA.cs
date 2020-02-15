9641311ad495e77c0e0cd2b51bd4176e241d5ef3
rogerioesilva <professor.rogerio@gmail.com>
2020-02-15 14:48:02 -0600

    City Wall + 2 ADA

---
+++ Medieval City VR/Assets/Scripts/ADA.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LocomotionState
+{
+    IDLE, WALKING, RUNNING
+};
+
+public class ADA : MonoBehaviour
+{
+    AnimationController _AnimController;
+    LocomotionState locState = LocomotionState.IDLE;
+    Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _AnimController = GetComponent<AnimationController>();
+        targetPosition = new Vector3(Random.Range());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
